c7b5ec4417dc664ca5bb499d203f270d70fcdac7
Pankaj Singh <pankajksingh189@gmail.com>
2018-06-10 12:07:37 +0530

    Addded testNG programs

---
+++ src/xmlDriven/Grouping.java
@@ -0,0 +1,29 @@
+package main;
+import org.testng.annotations.Test;
+
+public class Grouping {
+	@Test (groups = { "Car" })
+	public void Car1() {
+		System.out.println("Batch CAR - Test car 1");
+	}
+
+	@Test (groups = { "Train" })
+	public void Car2() {
+		System.out.println("Batch TRAIN - Test car 2");
+	}
+
+	@Test (groups = { "Scooter" })
+	public void Scooter1() {
+		System.out.println("Batch SCOOTER - Test scooter 1");
+	}
+
+	@Test (groups = { "Scooter","Bus" })
+	public void Scooter2() {
+		System.out.println("Batch SCOOTER BUS - Test scooter 2");
+	}
+
+	@Test (groups = { "Car", "Sedan Car" })
+	public void Sedan1() {
+		System.out.println("Batch SEDEN CAR - Test sedan 1");
+	}
+}
